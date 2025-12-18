a96ec7a67fa7cef47a463c05a229bfba49175d15
Asteropus <jamietheking07@proton.me>
2025-12-17 20:28:13 -0500

    version mismatch fix

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  webpack: (config) => {
-    // Ignore test, bench, and example files from node_modules
-    config.module.rules.push({
-      test: /node_modules.*\/(test|bench|example).*\.(js|mjs|ts)$/,
-      loader: 'ignore-loader',
-    });
-    
-    return config;
-  },
 };
 
 export default nextConfig;
