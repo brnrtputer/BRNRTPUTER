3dbb093b542853a203ed4b041e6202e890802d3e
Asteropus <jamietheking07@proton.me>
2025-12-17 19:43:03 -0500

    fix webpack test files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'why-is-node-running': false,
+      'tape': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
